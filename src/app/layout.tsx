8d0b7b25ef797f5a66dec9fc4b7fb9b34c004bec
Jhon <jhongdlp204@gmail.com>
2026-01-09 10:49:13 -0500

    OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,31 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://pet-season-color-demo.vercel.app/'),
   title: "The Chameleon Pet Shop",
   description: "A dynamic theming demo.",
+  openGraph: {
+    title: "The Chameleon Pet Shop",
+    description: "A dynamic theming demo showcasing vibrant colors and pets.",
+    url: 'https://pet-season-color-demo.vercel.app/',
+    siteName: 'The Chameleon Pet Shop',
+    images: [
+      {
+        url: '/images/OpenGraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'The Chameleon Pet Shop Hero Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "The Chameleon Pet Shop",
+    description: "A dynamic theming demo.",
+    images: ['/images/OpenGraph.png'],
+  },
 };
 
 export default function RootLayout({
